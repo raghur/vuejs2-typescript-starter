78302cce681f98a57325623b629d54a6d30dc403
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-08-25 23:42:48 +0530

    provide chunk name so that multiple async components are bundled in the same chunk

--- ClientApp/components/apps/app4.ts
+++ ClientApp/components/apps/app4.ts
@@ -1,26 +1,29 @@
 import  Vue  from 'vue';
+import {Component} from 'vue-property-decorator';
+import todoItem from './todo-item.vue.html';
 
-export default Vue.component('app4', {
-    data: () => {
-        return {
-            todos: [
-                {
-                    id: 0,
-                    text: "item 4"
-                },
-                {
-                    id: 1,
-                    text: "item 5"
-                },
-                {
-                    id: 2,
-                    text: "item 6"
-                }
-            ],
-            active: null
-        }
-    },
+@Component({
     components: {
-        "todo-item": () => new Promise((resolve, reject) => require(["./todo-item.vue.html"], resolve))
+        "todo-item":  () =>new Promise((resolve, reject) => require.ensure(["./todo-item.vue.html"], 
+                                                                            ()=> resolve(require("./todo-item.vue.html")), 
+                                                                            "comps"))
     }
-});
+})
+export default class App4 extends Vue{
+    todos= [
+        {
+            id: 0,
+            text: "item 4"
+        },
+        {
+            id: 1,
+            text: "item 5"
+        },
+        {
+            id: 2,
+            text: "item 6"
+        }
+    ]
+
+    active= null
+}
