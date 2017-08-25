a8b498cb1cf2ea2504aa165f0950eececd165811
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-08-25 22:08:05 +0530

    more changes.

--- ClientApp/components/apps/app4.ts
+++ ClientApp/components/apps/app4.ts
@@ -1,20 +1,26 @@
-import Vue from 'vue';
-// import todo from './todo-item.vue.html';
+import  Vue  from 'vue';
+
 export default Vue.component('app4', {
-    data: function() {
+    data: () => {
         return {
             todos: [
-                {id: 0,
-                    text: "item 4"},
-                {id: 1,
-                    text: "item 5"},
-                {id: 2,
-                    text: "item 6"}
+                {
+                    id: 0,
+                    text: "item 4"
+                },
+                {
+                    id: 1,
+                    text: "item 5"
+                },
+                {
+                    id: 2,
+                    text: "item 6"
+                }
             ],
             active: null
         }
     },
     components: {
-        "todo-item":  () => new Promise((resolve, reject) => require(["./todo-item.vue.html"], resolve))
+        "todo-item": () => new Promise((resolve, reject) => require(["./todo-item.vue.html"], resolve))
     }
 });
