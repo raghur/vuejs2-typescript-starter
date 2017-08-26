ea4008e8a396a84a4cdd8c7afa2695f594c00332
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-08-26 10:35:43 +0530

    Using promise.resolve for async comp

--- ClientApp/components/apps/app4.ts
+++ ClientApp/components/apps/app4.ts
@@ -1,13 +1,12 @@
 import  Vue  from 'vue';
 import {Component} from 'vue-property-decorator';
-import todoItem from './todo-item.vue.html';
 
 @Component({
     components: {
-        "todo-item":  () =>new Promise((resolve, reject) => require.ensure(["./todo-item.vue.html"], 
-                                                                            ()=> resolve(require("./todo-item.vue.html")), 
-                                                                            "comps"))
-    }
+        "todo-item":  () =>Promise.resolve(require.ensure(["./todo-item.vue.html"], 
+                                                            ()=> require("./todo-item.vue.html"), 
+                                                            "comps"))    
+        }
 })
 export default class App4 extends Vue{
     todos= [
