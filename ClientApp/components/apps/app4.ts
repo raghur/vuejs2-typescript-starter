4563bc00c78f62cfe745d8edc48eb8f66035df16
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-08-26 09:50:13 +0530

    trying to use import() and target same chunk

--- ClientApp/components/apps/app4.ts
+++ ClientApp/components/apps/app4.ts
@@ -2,11 +2,12 @@ import  Vue  from 'vue';
 import {Component} from 'vue-property-decorator';
 import todoItem from './todo-item.vue.html';
 
+        // "todo-item":  () =>new Promise((resolve, reject) => require.ensure(["./todo-item.vue.html"], 
+        //                                                                     ()=> resolve(require("./todo-item.vue.html")), 
+        //                                                                     "comps"))
 @Component({
     components: {
-        "todo-item":  () =>new Promise((resolve, reject) => require.ensure(["./todo-item.vue.html"], 
-                                                                            ()=> resolve(require("./todo-item.vue.html")), 
-                                                                            "comps"))
+        "todo-item":  () =>import(/* webpackChunkName = "comps" */ "./todo-item.vue.html" )
     }
 })
 export default class App4 extends Vue{
