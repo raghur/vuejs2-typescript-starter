3de6cd816ef0fab58c7976e079232ec31b5aae2c
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2017-08-28 12:02:32 +0530

    Use object literal syntax to define component

--- ClientApp/components/apps/app4.ts
+++ ClientApp/components/apps/app4.ts
@@ -1,28 +1,27 @@
-import  Vue  from 'vue';
-import {Component} from 'vue-property-decorator';
 
-@Component({
+export default {
+    data: () => ({
+        todos: [
+            {
+                id: 0,
+                text: "item 4"
+            },
+            {
+                id: 1,
+                text: "item 5"
+            },
+            {
+                id: 2,
+                text: "item 6"
+            }
+        ],
+        active: null
+    }),
+
     components: {
-        "todo-item":  () =>require.ensure(["./todo-item.vue.html"], 
-            () => require("./todo-item.vue.html"),
-            "comps")    
-        }
-})
-export default class App4 extends Vue{
-    todos= [
-        {
-            id: 0,
-            text: "item 4"
-        },
-        {
-            id: 1,
-            text: "item 5"
-        },
-        {
-            id: 2,
-            text: "item 6"
-        }
-    ]
-
-    active= null
+        "todo-item": () => require.ensure(
+                                    ["./todo-item.vue.html"],
+                                    () => require("./todo-item.vue.html"),
+                                    "comps")
+    }
 }
