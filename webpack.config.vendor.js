e73ef522dabc9d0e8d27ab7786cf2bbefcfe1882
Kuan Cheang <ku4ncheang@gmail.com>
2017-11-03 13:49:40 +0800

    [*] projects, renamed npm-shrinkwrap.json to project-lock.json. Remove the warning for npm install [*] tsconfig.json, Added types attribute. changed "strict" attribute to "false" since this will cause typescript compiler to throw an error about class component decorator. [*] packages, updated most of packages

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -18,10 +18,13 @@ module.exports = (env) => {
                 'isomorphic-fetch',
                 'jquery',
                 'vue',
-                'vue-router'
+                'vue-router',
+                'vuex'
             ],
         },
         module: {
+            exprContextRegExp: /$^/,
+            exprContextCritical: false,
             rules: [
                 { test: /\.css(\?|$)/, use: extractCSS.extract({ use: isDevBuild ? 'css-loader' : 'css-loader?minimize' }) },
                 { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
